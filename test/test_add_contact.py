81f39322e797f6d2d8b3c72240804e13d413e523
Anna <liska.jkl@gmail.com>
2021-06-27 23:50:11 +0300

    Методы логин и логаут вынесены в conftest

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,20 +11,15 @@ def setUp(self):
 
 
 def test_add_new_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="Ivan", middlename="Ivan", lastname="Ivanov",
                                nickname="ivanov", title="iv", company="IV", address="iviviv",
                                home="123", mobile="456", work="789", fax="987",
                                email="aaa@aa.com", email2="bbb@bb.com", email3="ccc@cc.com",
                                address2="aaaa", phone2="ssss", notes="dddd"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="",
                                title="", company="", address="", home="", mobile="",
                                work="", fax="", email="", email2="", email3="", address2="",
                                phone2="", notes=""))
-
-    app.session.logout()
--- test/test_update_contact.py
+++ test/test_update_contact.py
@@ -2,10 +2,8 @@ from model.contact import Contact
 
 
 def test_update_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.update_first_contact(Contact(firstname="Petr", middlename="Petr", lastname="Semaforov",
                                nickname="pet", title="ppp", company="pp", address="tgndgn",
                                home="111", mobile="222", work="333", fax="444",
                                email="aaa2@aa.com", email2="bbb2@bb.com", email3="ccc2@cc.com",
                                address2="bbc", phone2="666", notes="nzgngn"))
-    app.session.logout()
