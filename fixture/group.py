25ce373e689bdd2bf29892910f85596cfacfb178
Anna <liska.jkl@gmail.com>
2021-06-27 22:28:52 +0300

    Добавлен тест изменения первой группы

--- fixture/group.py
+++ fixture/group.py
@@ -33,6 +33,27 @@ class GroupHelper:
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
 
+    def update_first_group(self, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # open group for edit
+        wd.find_element_by_name("edit").click()
+        # change group properties
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys(group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys(group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        # save updates
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
     def open_new_group_page(self):
         wd = self.app.wd
         wd.find_element_by_xpath("//div[@id='content']/form/input[4]").click()
