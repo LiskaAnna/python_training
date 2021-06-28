ea1474726e09c1a941c156cf13fbae4a2070b919
Anna <liska.jkl@gmail.com>
2021-06-28 22:39:15 +0300

    Добавлены проверки в виде сравнение списков в методы работы с группами

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -4,6 +4,10 @@ def test_update_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
     old_groups = app.group.get_group_list()
-    app.group.update_first_group(Group(name="aaa", header="aaaa", footer="aaaaa"))
+    group = Group(name="aaa", header="aaaa", footer="aaaaa")
+    group.id = old_groups[0].id
+    app.group.update_first_group(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
