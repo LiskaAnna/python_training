83cb62a1b335f152795f3a5220ffdba69b45ff02
Anna <liska.jkl@gmail.com>
2021-06-28 21:45:39 +0300

    Добавлены проверки длины списка групп после операций добавления\удаления\изменения

--- test/test_update_group.py
+++ test/test_update_group.py
@@ -3,4 +3,7 @@ from model.group import Group
 def test_update_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.update_first_group(Group(name="aaa", header="aaaa", footer="aaaaa"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
