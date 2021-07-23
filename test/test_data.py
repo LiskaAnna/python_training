70201f1b9c36ebf18cff5303baa73e7b7978b060
Anna <liska.jkl@gmail.com>
2021-07-23 22:39:07 +0300

    Переделаны тесты для проверки информации о контактах на главной странице - сравниваются все записи с информацией из базы данных

--- test/test_data.py
+++ test/test_data.py
@@ -1,15 +1,13 @@
 import re
-from random import randrange
 
 
-def test_data_on_home_page(app):
-    index = randrange(len(app.contact.get_contact_list()))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
-    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
-    assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
-    assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+def test_data_on_home_page(app, db):
+    for element in app.contact.get_contact_list():
+        contact_from_db = db.get_contact_by_id(element.id)
+        assert element.firstname == contact_from_db.firstname
+        assert element.lastname == contact_from_db.lastname
+        assert element.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_db)
+        assert element.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_db)
 
 #def test_phones_on_contact_view_page(app):
 #   index = randrange(len(app.contact.get_contact_list()))
